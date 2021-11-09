0768d2952ce7b499a1549817069d344295555ff2
Tiago Moraes <tiago3902@gmail.com>
2021-11-09 01:20:51 -0300

    feat(crawler) implemented v2 of the heuristic

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -9,18 +9,18 @@ from time import sleep
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-# seeds=[('divinho', 'https://www.divinho.com.br'), 
-# ('divvino', 'https://www.divvino.com.br'), 
-# ('evino', 'https://www.evino.com.br'),
-# ('grandcru', 'https://www.grandcru.com.br'),
-# ('mistral', 'https://www.mistral.com.br'),
-# ('superadega', 'https://www.superadega.com.br'),
-# ('viavini', 'https://www.viavini.com.br'),
-# ('adegamais', 'https://adegamais.com.br'),
-# ('vivavinho', 'https://www.vivavinho.com.br'),
-# ('wine', 'https://www.wine.com.br')]
+seeds=[('divinho', 'https://www.divinho.com.br'), 
+('divvino', 'https://www.divvino.com.br'), 
+('evino', 'https://www.evino.com.br'),
+('grandcru', 'https://www.grandcru.com.br'),
+('mistral', 'https://www.mistral.com.br'),
+('superadega', 'https://www.superadega.com.br'),
+('viavini', 'https://www.viavini.com.br'),
+('adegamais', 'https://adegamais.com.br'),
+('vivavinho', 'https://www.vivavinho.com.br'),
+('wine', 'https://www.wine.com.br')]
 
-seeds = [('adegamais', 'https://adegamais.com.br')]
+# seeds=[('superadega', 'https://www.superadega.com.br')]
 
 def process_url(url, root):
     if (url.startswith('/')):
@@ -110,12 +110,39 @@ def bfs_crawl(seed):
 
 # ========== HEURISTIC ==========
 '''
-The baisc heuristic used here, finds all the anchors
-that have the word 'vinho' on the anchor text or href
+The baisc heuristic used here, prioritizes the wanted
+terms and discards non wanted terms on the text or href
 '''
+
+WANTED_TERMS = ['vinho', 'produto', 'product', 'tinto', 'branco', 
+'espumante', 'rose', 'rosado', 'cabernet', 'malbec', 'shiraz', 'primitivo', 'merlot', 
+'garnacha', 'pinot', 'carmenere', 'brut', 'mosacatel', 'tempranillo', 'reserva',
+'chardonnay', 'riesling', 'seco']
+
+NOT_WANTED_TERMS = ['kits?', 'saca', 'ta(ç|c)a', 'contato', 'sobre', 'cart', 
+'carrinho', 'termo', 'pol(í|i)tica', 'acess(ó|o)rios', 'blog', 'central']
+
+def classify_anchor(anchor):
+    for term in NOT_WANTED_TERMS:
+        if anchor.text and re.match(f'.*{term}.*', anchor.text.lower()):
+            return -1
+
+        if anchor['href'] and re.match(f'.*{term}.*', anchor['href'].lower()):
+            return -1
+
+    for term in WANTED_TERMS:
+        if anchor.text and re.match(f'.*{term}.*', anchor.text.lower()):
+            return 1
+        
+        if anchor['href'] and re.match(f'.*{term}.*', anchor['href'].lower()):
+            return 1
+        
+    return 0
+
+
 def save_and_get_page_urls_with_heuristic(start, seed, robots, count):
-    urls = set()
-    urls.add(start)
+    urls = [set(), set()]
+    urls[0].add(start)
 
     # Check headers if response is html text
     try:
@@ -143,43 +170,40 @@ def save_and_get_page_urls_with_heuristic(start, seed, robots, count):
     all_anchors = soup.find_all('a', href=True)
 
     for anchor in all_anchors:
-        # Only add anchors that have the word wine on it
-        if (anchor.find(string=re.compile('.*vinho.*', re.DOTALL)) or
-            anchor.find(string=re.compile('.*Vinho.*', re.DOTALL)) or
-            anchor.find(href=re.compile('.*vinho.*', re.DOTALL)) or
-            anchor.find(alt=re.compile('.*vinho.*', re.DOTALL)) or
-            anchor.find(string=re.compile('.*kit.*', re.DOTALL)) or
-            anchor.find(string=re.compile('.*tinto.*', re.DOTALL)) or
-            anchor.find(string=re.compile('.*branco.*', re.DOTALL)) or
-            anchor.find(string=re.compile('.*espumante.*', re.DOTALL)) or
-            anchor.find(string=re.compile('.*product.*', re.DOTALL)) or
-            anchor.find(string=re.compile('.*rose.*', re.DOTALL))
-        ):
-            url = anchor['href']
+        classification = classify_anchor(anchor)
+        url = anchor['href']
+        processed = process_url(url, seed[1])
 
-            # Check if url is usable
-            processed = process_url(url, seed[1])
+        # Check if url is usable
+        if (check_url(processed, seed[1], robots)):
+            if classification == 1:
+                urls[0].add(processed)
+            elif classification == 0:
+                urls[1].add(processed)
 
-            if (check_url(processed, seed[1], robots)):
-                urls.add(processed)
-    
     return urls
 
 
-def heuristic_crawl(seed, robots):
+def heuristic_crawl(seed):
+    robots = Robots.fetch(f'{seed[1]}/robots.txt')
+
     visited = set()
-    edge = [seed[1]]
+    edge = [[seed[1]], []]
     count = 0
 
-    while (len(edge) > 0 and len(visited) < 1000):
-        current = edge.pop(0)
+    while ((len(edge[0]) > 0 or len(edge[1]) > 0) and len(visited) < 1000):
+        if (len(edge[0]) > 0):
+            current = edge[0].pop(0)
+        else:
+            current = edge[1].pop(0)
 
         if (not current in visited):
             visited.add(current)
             count += 1
 
             new_urls = save_and_get_page_urls_with_heuristic(current, seed, robots, count)
-            edge.extend(new_urls)
+            edge[0].extend(new_urls[0])
+            edge[1].extend(new_urls[1])
             sleep(0.01)
 
             print(f'Entered {current}')
@@ -189,7 +213,7 @@ def heuristic_crawl(seed, robots):
 
 def main():
     pool = Pool(10)
-    pool.map(bfs_crawl, seeds)
+    pool.map(heuristic_crawl, seeds)
     pool.close()
     pool.join()
 
